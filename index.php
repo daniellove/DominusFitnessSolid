675a359895c85eeb2bd51c906c8a91b4fb4fda3d
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2022-02-05 11:13:30 +1300

    moved head to php elements

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <!DOCTYPE html>
 <html translate="no">
-<head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans" />
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-	<link rel="stylesheet" type="text/css" href="files/styles.css">
-	<script type="text/javascript" src="files/script.js"></script>
-</head>
+<?php include '/php-elements/head.php' ?>
 <body>
 
-	<?php include 'nav.php'; ?>
+	<?php include '/php-elements/nav.php'; ?>
 	<!-- <div id="topNav" class="container fixed">
 		<div class="nav">
 			<div id="menu">
