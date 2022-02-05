0bf6cb9f0862ad6ce9a5db91a3dff2367f895901
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2022-02-05 17:59:39 +1300

    Added pages and links from menu

---
+++ pages/signup/index.php
@@ -0,0 +1,3 @@
+<html translate="no">
+<?php include($_SERVER['DOCUMENT_ROOT'].'/DominusFitness/php-elements/head.php'); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/DominusFitness/php-elements/nav.php'); ?>
--- php-elements/menu.php
+++ php-elements/menu.php
@@ -1,4 +1,6 @@
-<div id="menu">
-	<p>asd</p>
-	<p>asd</p>
+<div id="menuEle">
+	<span onclick="window.location = '/DominusFitness/pages/signup/'">Sign Up <i class="fas fa-arrow-right"></i></span>
+	<span onclick="window.location = '/DominusFitness/pages/testimonials/'">Testimonials <i class="fas fa-arrow-right"></i></span>
+	<span onclick="window.location = '/DominusFitness/pages/about/'">About <i class="fas fa-arrow-right"></i></span>
+	<span onclick="window.location = '/DominusFitness/pages/contact/'">Contact <i class="fas fa-arrow-right"></i></span>
 </div>
